718344794d928999ea40ec0a9b004e0053730f9b
Fredrik Hansson <fredda.hansson@gmail.com>
2020-04-11 22:53:24 +0200

    Plot interactively and with methods

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-t0 = time.time()
-x_min, x_max = -2.2, -0.6
-y_min, y_max = -0.5, 0.5
-N = 10**4
 
-max_iterations=100
-
-def mandelbrot(c, max_iterations): 
+def get_stability(c, max_iterations=100): 
+    """
+    Calculate z_{n+1} = z_{n}**2 + c and return the number of iterations until abs(z) > 2
+    """
     z = 0
     for i in range(max_iterations):
         z = z**2 + c
@@ -17,16 +14,53 @@ def mandelbrot(c, max_iterations):
             return i
     return i
 
-x = np.linspace(x_min, x_max, N)
-y = np.linspace(y_min, y_max, N)
 
-mb = np.zeros((len(y), len(x)))
-for i in range(len(x)):
-    for j in range(len(y)):
-        c = complex(x[i], y[j])
-        mb[j,i] = mandelbrot(c, max_iterations)
+def calc_mb_set(xlim=[-2.5, 1.5], ylim=[-1.5, 1.5], N=None, max_iterations=100):
+    """
+    Calculate the entire mandelbrot set within xlim and ylim. Return the set
+    in an ndarray.
+    """
 
-plt.imshow(mb, cmap='gnuplot2', interpolation='bilinear', origin='lower', extent=[x_min, x_max, y_min, y_max])
+    if not N:
+        nx, ny=600, 600
+    elif len(N)==1:
+        nx, ny = N, N
+    elif len(N)==2:
+        nx, ny = N[0], N[1]
 
-print(N, time.time()-t0)
-plt.show()
+    xlim = sorted(xlim)
+    ylim = sorted(ylim)
+    x = np.linspace(xlim[0], xlim[1], nx)
+    y = np.linspace(ylim[0], ylim[1], ny)
+    
+    mb_set = np.zeros((len(y), len(x)))
+    for i in range(len(x)):
+        for j in range(len(y)):
+            c = complex(x[i], y[j])
+            mb_set[j,i] = get_stability(c, max_iterations)
+
+    return mb_set
+
+def plot_mb_set(mb_set, xlim=[-2.5, 1.5], ylim=[-1.5, 1.5]):
+
+    fig = plt.figure()
+
+    mng = plt.get_current_fig_manager()
+    mng.window.showMaximized()
+    
+    cmap = ['RdGy', 'seismic', 'jet', 'cividis', 'inferno', 'plasma', 'binary', 'PuBu', 'Blues', 'Greys']
+    im = plt.imshow(mb_set, cmap=cmap[1], interpolation='bilinear', origin='lower', extent=[xlim[0], xlim[1], ylim[0], ylim[1]])
+    plt.tight_layout()
+
+    return im, cmap
+
+
+
+
+if __name__ == '__main__':
+
+    # Enable interactive plotting
+    plt.ion()
+
+    mb_set = calc_mb_set()
+    im, cmap = plot_mb_set(mb_set)
